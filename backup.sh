ce1adec0dce1dc20e4784d30e827080c2847c8ca
stiench <sebastien.gerber@outlook.com>
2026-04-01 04:17:20 +0200

    Backup config

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cp ~/git/MagicMirror/config.js ./
+cp ~/git/MagicMirror/config/config.js ./
 
 perl -i -pe 's#(\burl\s*:\s*")[^"]*(")#${1}REDACTED_URL${2}#g' ./config.js
---
+++ config.js
@@ -0,0 +1,132 @@
+/* Config Sample
+ *
+ * For more information on how you can configure this file
+ * see https://docs.magicmirror.builders/configuration/introduction.html
+ * and https://docs.magicmirror.builders/modules/configuration.html
+ *
+ * You can use environment variables using a `config.js.template` file instead of `config.js`
+ * which will be converted to `config.js` while starting. For more information
+ * see https://docs.magicmirror.builders/configuration/introduction.html#enviromnent-variables
+ */
+let config = {
+	address: "0.0.0.0",	// Address to listen on, can be:
+							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+							// - another specific IPv4/6 to listen on a specific interface
+							// - "0.0.0.0", "::" to listen on any interface
+							// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
+	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
+									// you must set the sub path here. basePath must end with a /
+	ipWhitelist: [],	// Set [] to allow all IP addresses
+									// or add a specific IPv4 of 192.168.1.5 :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+
+	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
+	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
+	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+
+	language: "fr",
+	locale: "en-US",   // this variable is provided as a consistent location
+			   // it is currently only used by 3rd party modules. no MagicMirror code uses this value
+			   // as we have no usage, we  have no constraints on what this field holds
+			   // see https://en.wikipedia.org/wiki/Locale_(computer_software) for the possibilities
+
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	timeFormat: 24,
+	units: "metric",
+
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "MMM-CalendarExt3",
+			position: "middle_center",
+			title: "Calendrier",
+			config: {
+				mode: "week",
+				instanceId: "basicCalendar",
+				locale: 'fr-CH',
+				weekIndex: 0,
+				weeksInView: 4,
+				maxEventLines: 5,
+				firstDayOfWeek: 1,
+				calendarSet: ['gerber', 'feries']
+			}
+		},
+		{
+			module: "calendar",
+			config: {
+        		broadcastPastEvents: true, 
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "REDACTED_URL",
+						name: "gerber",
+            			color: "orange"
+					}
+				],	
+			}	
+		},
+		{
+			module: "calendar",
+			config: {
+        		broadcastPastEvents: true, 
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "REDACTED_URL",
+						name: "feries",
+            			color: "green"
+					}
+				],	
+			}	
+		},
+		{
+			module: 'MMM-OpenWeatherMapForecast',
+			header: "Weather",
+			position: "top_right",
+			config: {
+				apikey: '2bb84df4266fb0e52065afd0cd224b3d',
+				latitude: 46.992008,
+				longitude: 6.931944,
+				iconset: "4c",
+				concise: false,
+				forecastLayout: "table",
+				units: "metric",
+				updateInterval: 600000,
+				showCurrentConditions: true,
+				showForecast: true
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "RTS",
+						url: "REDACTED_URL"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") { module.exports = config; }
